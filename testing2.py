8e9cf6f3415f4578714aba00870dd99d9e7ce808
akmaulana33 <akmaljr33@gmail.com>
2025-05-19 13:25:43 +0700

    tambah tambahan

---
+++ testing2.py
@@ -0,0 +1,11 @@
+# Kalkulator Penambahan Dua Angka
+
+# Input dari pengguna
+angka1 = float(input("Masukkan angka pertama: "))
+angka2 = float(input("Masukkan angka kedua: "))
+
+# Proses penambahan
+hasil = angka1 + angka2
+
+# Tampilkan hasil
+print("Hasil penambahan:", hasil)
